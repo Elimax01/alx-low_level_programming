c1470f8558c9bc342a2f12a23855d26e84263892
Elimax01 <89745740+Elimax01@users.noreply.github.com>
2022-06-30 09:18:43 -0800

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/* the first code 
+/* 
+ *the first code 
  * output as follows
 */
 int main(void)
 {
-	puts("\"Programing is like building a multilingual puzzle");
+	puts("\"Programing is like building a multilingual puzzle/n");
 	return(0);
 }
