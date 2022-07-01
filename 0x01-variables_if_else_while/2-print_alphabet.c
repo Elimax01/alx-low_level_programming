d2761aa8e4bd533a6e0eaca95969e4b85345b7a5
Elias Afework <eliasafework999@gmail.com>
2022-07-01 12:17:35 -0500

    if and else statments

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+
+* main page
+*
+* main accesse
+*/
+
+int main(void)
+
+{
+	char alp;
+
+	for (alp = 'a'; alp <='z'; alp ++)
+		putchar(alp);
+		putchar('\n');
+	return (0);
+}
