8462d97c12e1685e58766693d4373eb8b445ccca
Elias Afework <eliasafework999@gmail.com>
2022-07-11 11:57:02 -0500

    pomters and arrays

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+* _strlen -> function to get the length
+*
+* @s: string pointer
+* Return: the length
+*/
+
+int _strlen(char *s)
+{
+	return(strlen(s));
+}
