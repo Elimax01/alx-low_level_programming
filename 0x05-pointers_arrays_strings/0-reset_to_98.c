8744ba9031342eb129b592e8d61870ad250d1f0d
Elias Afework <eliasafework999@gmail.com>
2022-07-11 11:25:49 -0500

    pomters and arrays

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* reset it to 98-> a function update the referance
+* reset_to_98 -> a function update the referance
 *
-* Return: Always 0.
+* @n: pointer to n to be updated
 */
 void reset_to_98(int *n)
 {
