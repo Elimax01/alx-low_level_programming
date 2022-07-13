c1f33bdc6c9d75bd7681b448ad889bb313eb6943
Elias Afework <eliasafework999@gmail.com>
2022-07-13 12:31:33 -0500

    ponters and arrays 2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strncat -> function to append
+* @dest: first par
+* @src: second par
+* @n: 3rd par
+* Return: string
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n)
+	return (dest);
+}
